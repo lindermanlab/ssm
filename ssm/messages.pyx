29958e51001696fe8737e8db8fea67a69f052d39
Scott Linderman <scott.linderman@gmail.com>
2019-04-05 18:05:40 -0700

    first stab at a low memory implementation of EM for stationary transition model

--- ssm/messages.pyx
+++ ssm/messages.pyx
@@ -39,13 +39,6 @@ cdef dlse(double[::1] a,
         out[k] = exp(a[k] - lse)
 
 
-# def dLSE_da(a, B):
-#     return np.exp(a + B.T - logsumexp(a + B.T, axis=1, keepdims=True))
-#
-# def vjp_LSE_B(a, B, v):
-#     return v * dLSE_da(a, B).T
-
-
 cpdef forward_pass(double[::1] log_pi0,
                    double[:,:,::1] log_Ps,
                    double[:,::1] log_likes,
@@ -54,7 +47,7 @@ cpdef forward_pass(double[::1] log_pi0,
     cdef int T, K, t, k, j
     T = log_likes.shape[0]
     K = log_likes.shape[1]
-    assert log_Ps.shape[0] == T-1
+    assert log_Ps.shape[0] == T-1 or log_Ps.shape[0] == 1
     assert log_Ps.shape[1] == K
     assert log_Ps.shape[2] == K
     assert alphas.shape[0] == T
@@ -62,13 +55,16 @@ cpdef forward_pass(double[::1] log_pi0,
 
     cdef double[::1] tmp = np.zeros(K)
 
+    # Trick for handling time-varying transition matrices
+    cdef int hetero = (log_Ps.shape[0] == T-1)
+
     for k in range(K):
         alphas[0, k] = log_pi0[k] + log_likes[0, k]
 
     for t in range(T - 1):
         for k in range(K):
             for j in range(K):
-                tmp[j] = alphas[t, j] + log_Ps[t, j, k]
+                tmp[j] = alphas[t, j] + log_Ps[t * hetero, j, k]
             alphas[t+1, k] = logsumexp(tmp) + log_likes[t+1, k]
 
     return logsumexp(alphas[T-1])
@@ -80,13 +76,16 @@ cpdef backward_pass(double[:,:,::1] log_Ps,
     cdef int T, K, t, k, j
     T = log_likes.shape[0]
     K = log_likes.shape[1]
-    assert log_Ps.shape[0] == T-1
+    assert log_Ps.shape[0] == T-1 or log_Ps.shape[0] == 1
     assert log_Ps.shape[1] == K
     assert log_Ps.shape[2] == K
     assert betas.shape[0] == T
     assert betas.shape[1] == K
 
     cdef double[::1] tmp = np.zeros(K)
+
+    # Trick for handling time-varying transition matrices
+    cdef int hetero = (log_Ps.shape[0] == T-1)
 
     # Initialize the last output
     for k in range(K):
@@ -96,7 +95,7 @@ cpdef backward_pass(double[:,:,::1] log_Ps,
         # betal[t] = logsumexp(Al + betal[t+1] + aBl[t+1],axis=1)
         for k in range(K):
             for j in range(K):
-                tmp[j] = log_Ps[t, k, j] + betas[t+1, j] + log_likes[t+1, j]
+                tmp[j] = log_Ps[t * hetero, k, j] + betas[t+1, j] + log_likes[t+1, j]
             betas[t, k] = logsumexp(tmp)
 
 
@@ -111,7 +110,7 @@ cpdef backward_sample(double[:,:,::1] log_Ps,
 
     T = log_likes.shape[0]
     K = log_likes.shape[1]
-    assert log_Ps.shape[0] == T-1
+    assert log_Ps.shape[0] == T-1 or log_Ps.shape[0] == 1
     assert log_Ps.shape[1] == K
     assert log_Ps.shape[2] == K
     assert alphas.shape[0] == T
@@ -121,6 +120,9 @@ cpdef backward_sample(double[:,:,::1] log_Ps,
 
     cdef double[::1] lpzp1 = np.zeros(K)
     cdef double[::1] lpz = np.zeros(K)
+
+    # Trick for handling time-varying transition matrices
+    cdef int hetero = (log_Ps.shape[0] == T-1)
 
     for t in range(T-1,-1,-1):
         # compute normalized log p(z[t] = k | z[t+1])
@@ -140,7 +142,7 @@ cpdef backward_sample(double[:,:,::1] log_Ps,
         # set the transition potential
         if t > 0:
             for k in range(K):
-                lpzp1[k] = log_Ps[t-1, k, zs[t]]
+                lpzp1[k] = log_Ps[(t-1) * hetero, k, zs[t]]
 
 
 cpdef grad_hmm_normalizer(double[:,:,::1] log_Ps,
@@ -195,6 +197,3 @@ cpdef grad_hmm_normalizer(double[:,:,::1] log_Ps,
     # d_log_pi0 = d_log_likes[0]
     for k in range(K):
         d_log_pi0[k] = d_log_likes[0, k]
-
-
-# def grad_cholesky_banded
