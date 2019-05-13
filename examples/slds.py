fixing small bugs in hessian.  splitting the laplace em code into a few functions. adding some helper functions for block tridiag and newtons method. playing with slds demo

examples/slds.py
@@ -6,87 +6,115 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 import ssm
-from ssm.variational import SLDSMeanFieldVariationalPosterior, SLDSTriDiagVariationalPosterior
+from ssm.variational import SLDSMeanFieldVariationalPosterior, SLDSTriDiagVariationalPosterior, \
+	SLDSStructuredMeanFieldVariationalPosterior
 from ssm.util import random_rotation, find_permutation
 
 # Set the parameters of the HMM
-T = 1000    # number of time bins
+T = 10000   # number of time bins
 K = 5       # number of discrete states
 D = 2       # number of latent dimensions
-N = 10      # number of observed dimensions
+N = 50      # number of observed dimensions
 
 # Make an SLDS with the true parameters
-true_slds = ssm.SLDS(N, K, D, emissions="gaussian")
+true_slds = ssm.SLDS(N, K, D, emissions="bernoulli_orthog")
 for k in range(K):
     true_slds.dynamics.As[k] = .95 * random_rotation(D, theta=(k+1) * np.pi/20)
+    # true_slds.dynamics.bs[k] = .1 * npr.randn(D)
+# true_slds.dynamics.Sigmas = np.tile(0.1 * np.eye(D)[None, :, :], (K, 1, 1))
 
 # Sample training and test data from the SLDS
 z, x, y = true_slds.sample(T)
 z_test, x_test, y_test = true_slds.sample(T)
 
-# Mask off some data
-mask = npr.rand(T, N) < 0.75
-y_masked = y * mask
-
 # Fit an SLDS with mean field posterior
 print("Fitting SLDS with SVI using structured variational posterior")
-slds = ssm.SLDS(N, K, D, emissions="gaussian")
-slds.initialize(y_masked, masks=mask)
+slds = ssm.SLDS(N, K, D, emissions="bernoulli")
+slds.initialize(y)
 
-q_mf = SLDSMeanFieldVariationalPosterior(slds, y_masked, masks=mask)
-q_mf_elbos = slds.fit(q_mf, y_masked, masks=mask, num_iters=1000, initialize=False)
-q_mf_x = q_mf.mean[0]
-q_mf_y = slds.smooth(q_mf_x, y)
+# q_mf = SLDSMeanFieldVariationalPosterior(slds, y_masked, masks=mask)
+# q_mf_elbos = slds.fit(q_mf, y_masked, masks=mask, num_iters=1000, initialize=False)
+# q_mf_x = q_mf.mean[0]
+# q_mf_y = slds.smooth(q_mf_x, y)
+
+# # Find the permutation that matches the true and inferred states
+# slds.permute(find_permutation(z, slds.most_likely_states(q_mf_x, y)))
+# q_mf_z = slds.most_likely_states(q_mf_x, y)
+
+# # Do the same with the structured posterior
+# print("Fitting SLDS with SVI using structured variational posterior")
+# slds = ssm.SLDS(N, K, D, emissions="gaussian")
+# slds.initialize(y_masked, masks=mask)
+
+# q_struct = SLDSTriDiagVariationalPosterior(slds, y_masked, masks=mask)
+# q_struct_elbos = slds.fit(q_struct, y_masked, masks=mask, num_iters=1000, initialize=False)
+# q_struct_x = q_struct.mean[0]
+# q_struct_y = slds.smooth(q_struct_x, y)
+
+# # Find the permutation that matches the true and inferred states
+# slds.permute(find_permutation(z, slds.most_likely_states(q_struct_x, y)))
+# q_struct_z = slds.most_likely_states(q_struct_x, y)
+
+q_laplace_em = SLDSStructuredMeanFieldVariationalPosterior(slds, y)
+q_lem_elbos = slds.fit(q_laplace_em, y, method="laplace_em", num_iters=100, initialize=False)
+q_lem_Ez, q_lem_x = q_laplace_em.mean[0]
+q_lem_y = slds.smooth(q_lem_x, y)
 
 # Find the permutation that matches the true and inferred states
-slds.permute(find_permutation(z, slds.most_likely_states(q_mf_x, y)))
-q_mf_z = slds.most_likely_states(q_mf_x, y)
+slds.permute(find_permutation(z, slds.most_likely_states(q_lem_x, y)))
+q_lem_z = slds.most_likely_states(q_lem_x, y)
 
-# Do the same with the structured posterior
-print("Fitting SLDS with SVI using structured variational posterior")
-slds = ssm.SLDS(N, K, D, emissions="gaussian")
-slds.initialize(y_masked, masks=mask)
-
-q_struct = SLDSTriDiagVariationalPosterior(slds, y_masked, masks=mask)
-q_struct_elbos = slds.fit(q_struct, y_masked, masks=mask, num_iters=1000, initialize=False)
-q_struct_x = q_struct.mean[0]
-q_struct_y = slds.smooth(q_struct_x, y)
-
-# Find the permutation that matches the true and inferred states
-slds.permute(find_permutation(z, slds.most_likely_states(q_struct_x, y)))
-q_struct_z = slds.most_likely_states(q_struct_x, y)
+# Linearly transform the x's to match the true x's
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(q_lem_x, x)
+q_lem_x_trans = lr.predict(q_lem_x)
 
 # Plot the ELBOS
 plt.figure()
-plt.plot(q_mf_elbos, label="MF")
-plt.plot(q_struct_elbos, label="LDS")
+plt.plot(q_lem_elbos, label="MF")
+# plt.plot(q_struct_elbos, label="LDS")
 plt.xlabel("Iteration")
 plt.ylabel("ELBO")
 plt.legend()
+plt.tight_layout()
 
 # Plot the true and inferred states
-plt.figure(figsize=(8,6))
+plt.figure(figsize=(8,9))
 xlim = (0, 1000)
 
-plt.subplot(311)
-plt.imshow(np.row_stack((z, q_mf_z, q_struct_z)), aspect="auto")
-plt.yticks([0, 1, 2], ["$z_{{\\mathrm{{true}}}}$", "$z_{{\\mathrm{{mf}}}}$", "$z_{{\\mathrm{{lds}}}}$"])
+plt.subplot(411)
+plt.imshow(z[None, :], aspect="auto")
+# plt.imshow(np.row_stack((z, q_mf_z, q_struct_z)), aspect="auto")
+plt.imshow(np.row_stack((z, q_lem_z)), aspect="auto")
+# plt.yticks([0, 1, 2], ["$z_{{\\mathrm{{true}}}}$", "$z_{{\\mathrm{{mf}}}}$", "$z_{{\\mathrm{{lds}}}}$"])
+plt.yticks([0, 1, 2], ["$z_{{\\mathrm{{true}}}}$", "$z_{{\\mathrm{{L. EM}}}}$"])
 plt.xlim(xlim)
+plt.title("True and Most Likely Inferred States")
 
-plt.subplot(312)
+plt.subplot(412)
+plt.imshow(q_lem_Ez[0].T, aspect="auto", cmap="Greys")
+plt.xlim(xlim)
+plt.title("Inferred State Probability")
+
+plt.subplot(413)
 plt.plot(x, '-k', label="True")
-plt.plot(q_mf_x, '--b', label="$q_{\\text{MF}}$")
-plt.plot(q_struct_x, ':r', label="$q_{\\text{LDS}}$")
+# plt.plot(q_mf_x, '--b', label="$q_{\\text{MF}}$")
+# plt.plot(q_struct_x, ':r', label="$q_{\\text{LDS}}$")
+plt.plot(q_lem_x_trans, ':r', label="$q_{\\text{Laplace}}$")
 plt.ylabel("$x$")
 plt.xlim(xlim)
 
-plt.subplot(313)
+plt.subplot(414)
+spc = 1.1 * abs(y).max()
 for n in range(N):
-    plt.plot(y[:, n] + 4 * n, '-k', label="True" if n == 0 else None)
-    plt.plot(q_mf_y[:, n] + 4 * n, '--b', label="MF" if n == 0 else None)
-    plt.plot(q_struct_y[:, n] + 4 * n, ':r', label="LDS" if n == 0 else None)
+    plt.plot(y[:, n] - spc * n, '-k', label="True" if n == 0 else None)
+    # plt.plot(q_mf_y[:, n] - spc * n, '--b', label="MF" if n == 0 else None)
+    # plt.plot(q_struct_y[:, n] - spc * n, ':r', label="LDS" if n == 0 else None)
+    plt.plot(q_lem_y[:, n] - spc * n, '--b', label="Laplace EM" if n == 0 else None)
 plt.legend()
 plt.ylabel("$y$")
+plt.yticks(-spc * np.arange(N), ["$y_{}$".format(n+1) for n in range(N)])
 plt.xlabel("time")
 plt.xlim(xlim)
 

ssm/util.py
@@ -9,6 +9,7 @@ from autograd import grad
 from scipy.optimize import linear_sum_assignment, minimize
 from scipy.special import gammaln, digamma, polygamma
 
+from ssm.primitives import solve_symm_block_tridiag
 
 def compute_state_overlap(z1, z2, K1=None, K2=None):
     assert z1.dtype == int and z2.dtype == int
@@ -28,7 +29,6 @@ def compute_state_overlap(z1, z2, K1=None, K2=None):
 def find_permutation(z1, z2, K1=None, K2=None):
     overlap = compute_state_overlap(z1, z2, K1=K1, K2=K2)
     K1, K2 = overlap.shape
-    assert K1 <= K2, "Can only find permutation from more states to fewer"
 
     tmp, perm = linear_sum_assignment(-overlap)
     assert np.all(tmp == np.arange(K1)), "All indices should have been matched!"
@@ -398,7 +398,6 @@ def fit_linear_regression(Xs, ys, weights=None,
 
     # Get MAP estimate of posterior covariance
     Sigma = Psi / (nu + P + 1)
-
     if fit_intercept:
         return W, b, Sigma
     else:
@@ -432,4 +431,33 @@ def fit_negative_binomial_integer_r(xs, r_min=1, r_max=20):
     r_star = rs[np.argmax(mlls)]
 
     return r_star, p_star(r_star)
+
+
+def newtons_method_block_tridiag_hessian(
+    x0, obj, grad_func, hess_func,
+    stepsize=0.95, tolerance=1e-4, maxiter=100):
+    """
+    Newton's method to minimize a positive definite function with a
+    block tridiagonal Hessian matrix.
+    """
+    from ssm.primitives import blocks_to_full
+    x = x0
+    is_converged = False
+    count = 0
+    while not is_converged:
+        # print("Objective: ", obj(x))
+        H_diag, H_lower_diag = hess_func(x)
+        g = grad_func(x)
+        dx = solve_symm_block_tridiag(H_diag, H_lower_diag, g)
+        x = x - stepsize * dx
+        is_converged = np.mean(np.abs(dx)) < tolerance
+        count += 1
+        if count > maxiter:
+            break
+
+    if not is_converged:
+        warn("Newton's method failed to converge in {} iterations. "
+             "Final mean abs(dx): {}".format(maxiter, np.mean(np.abs(dx))))
+
+    return x
 

ssm/variational.py
@@ -227,7 +227,8 @@ class SLDSStructuredMeanFieldVariationalPosterior(VariationalPosterior):
     """
     @ensure_variational_args_are_lists
     def __init__(self, model, datas,
-                 inputs=None, masks=None, tags=None):
+                 inputs=None, masks=None, tags=None,
+                 initial_variance=1):
 
         super(SLDSStructuredMeanFieldVariationalPosterior, self).\
             __init__(model, datas, masks, tags)
@@ -237,6 +238,7 @@ class SLDSStructuredMeanFieldVariationalPosterior(VariationalPosterior):
         self.D = model.D
         self.K = model.K
         self.Ts = [data.shape[0] for data in datas]
+        self.initial_variance = initial_variance
         self._params = [self._initialize_variational_params(data, input, mask, tag)
                        for data, input, mask, tag in zip(datas, inputs, masks, tags)]
 
@@ -248,13 +250,13 @@ class SLDSStructuredMeanFieldVariationalPosterior(VariationalPosterior):
         # Initialize q(z) parameters: log_pi0, log_likes, log_transition_matrices
         log_pi0 = -np.log(K) * np.ones(K)
         log_Ps = np.zeros((T-1, K, K))
-        log_likes = 0.1 * npr.randn(T, K)
+        log_likes = np.zeros((T, K))
 
         # Initialize q(x) = = N(J, h) where J is block tridiagonal precision
         # and h is the linear potential.  The mapping to mean parameters is
         # mu = J^{-1} h and Sigma = J^{-1}.  Initialize J to be identity so
         # that h is the mean.
-        J_diag = np.tile(np.eye(D)[None, :, :], (T, 1, 1))
+        J_diag = np.tile(self.initial_variance * np.eye(D)[None, :, :], (T, 1, 1))
         J_lower_diag = np.zeros((T-1, D, D))
         h = self.model.emissions.invert(data, input=input, mask=mask, tag=tag)
 
@@ -290,7 +292,7 @@ class SLDSStructuredMeanFieldVariationalPosterior(VariationalPosterior):
 
     @property
     def mean(self):
-        return list(zip(self.mean_discrete_states(), self.mean_continuous_states()))
+        return list(zip(self.mean_discrete_states, self.mean_continuous_states))
 
     def log_density(self, sample):
         raise NotImplementedError