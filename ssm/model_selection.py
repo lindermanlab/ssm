9985b609e05f9e86d48ea3715e0e19a012124d5d
ahwillia <alex.h.willia@gmail.com>
2019-06-20 10:56:11 -0700

    pair with scott

--- ssm/model_selection.py
+++ ssm/model_selection.py
@@ -4,19 +4,23 @@ Cross-validation and related model diagnostics.
 import autograd.numpy as np
 import autograd.numpy.random as npr
 
+from util import ensure_args_are_lists
 
+
+@ensure_args_are_lists
 def cross_val_scores(
-        model, data, inputs=None, masks=None, tags=None,
-        heldout_frac=0.1, n_repeats=3, normalize_scores=True):
+        model, datas, inputs=None, masks=None, tags=None,
+        heldout_frac=0.1, n_repeats=3):
     """
-    Evaluate HMM log-likelihood scores on heldout data.
+    Evaluate HMM log-likelihood scores on heldout data using
+    a speckled holdout pattern.
 
     Parameters
     ----------
     model : HMM
         Model instance.
-    data : ndarray
-        Data matrix with shape (n_obs, n_out).
+    datas : ndarray or list of ndarray
+        Data matrices with shape (n_obs, n_out).
     inputs : ndarray
         If applicable, matrix holding input variables with shape
         (n_obs, n_in).
@@ -31,75 +35,56 @@ def cross_val_scores(
         to hold out on each cross-validation run.
     n_repeats : int
         Number of randomized cross-validation runs to perform.
-    normalize_scores : bool
-        If True, normalize log-likelihood scores per observation.
-        Defaults to True.
 
     Returns
     -------
     test_scores : ndarray
-        Array holding log-likelihood scores on test set.
-        Has shape (n_repeats,).
+        Array holding normalized log-likelihood scores on test
+        set. Has shape (n_repeats,).
     train_scores : ndarray
         Array holding log-likelihood scores on training set.
         Has shape (n_repeats,).
     """
 
-    # Initialize mask for missing data. By default all data
-    # is observed.
-    if masks is None:
-        masks = np.ones_like(data, dtype=bool)
-    else:
-        masks = np.asarray(masks, dtype=bool)
-
-    # Total number of observations and indices of observed data.
-    total_obs = np.sum(masks)
-    obs_ind = np.argwhere(masks)
-
-    # Determine number of observations to holdout.
-    heldout_num = int(total_obs * heldout_frac)
-
     # Allocate space for train and test log-likelihoods.
     test_scores = np.empty(n_repeats)
     train_scores = np.empty(n_repeats)
 
-    # Ensure inputs and tags are compatible with iteration.
-    M = (model.M,) if isinstance(model.M, int) else model.M
-    if inputs is None:
-        inputs = np.zeros((data.shape[0],) + M)
-    if tags is None:
-        tags = [None]
-
     for r in range(n_repeats):
 
-        # Randomly choose indices to hold out.
-        i = npr.choice(total_obs, heldout_num, replace=False)
-        heldout_ind = obs_ind[i]
-
         # Create mask for training data.
-        train_mask = np.copy(masks)
-        train_mask[heldout_ind] = False
+        train_masks = []
+        for k, m in enumerate(masks):
 
-        # Fit model.
-        model.fit(data, inputs=inputs, masks=train_mask)
+            # Determine number of heldout points.
+            total_obs = np.sum(m)
+            obs_inds = np.argwhere(m)
+            heldout_num = int(total_obs * heldout_frac)
 
-        # Compute expectations of hidden states.
-        expectations = [model.expected_states(x, inp, m, tg)
-                        for x, inp, m, tg
-                        in zip([data], inputs, train_mask, tags)]
+            # Randomly hold out speckled data pattern.
+            heldout_flat_inds = npr.choice(
+                total_obs, heldout_num, replace=False)
 
-        # Evaluate loss on training data.
-        train_scores[r] = model.expected_log_likelihood(
-            expectations, data, inputs=inputs, masks=train_mask, tags=tags)
+            heldout_inds = obs_inds[heldout_flat_inds]
 
-        # Compute log-likelihood on test data.
-        test_mask = ~train_mask & masks
-        test_scores[r] = model.log_likelihood(
-            expectations, data, inputs=inputs, masks=test_mask, tags=tags)
+            # Create training mask.
+            train_masks.append(m.copy())
+            train_masks[-1][heldout_ind] = False
 
-    # Normalize log-likelihoods per observation.
-    if normalize_scores:
-        train_scores /= train_mask.sum()
-        test_scores /= test_mask.sum()
+        # Fit model with training mask.
+        lls = model.fit(data, inputs=inputs, tags=tags, masks=train_masks)
+        train_ll = lls[-1]
+
+        # Compute log-likelihood without training mask.
+        full_ll = model.log_likelihood(
+            data, inputs=inputs, tags=tags, masks=masks)
+
+        # Total number of training and observed datapoints.
+        n_observed = np.sum([m.sum() for m in masks])
+        n_train = np.sum([m.sum() for m in train_masks])
+
+        # Save normalized log-likelihood scores.
+        test_scores[r] = (full_ll - train_ll) / (n_observed - n_train)
+        train_scores[r] = train_ll / n_train
 
     return test_scores, train_scores
