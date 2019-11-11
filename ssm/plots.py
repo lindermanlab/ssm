7d8eef45253472bcd0603f58655356bab138a104
Benjamin Antin <bantin@stanford.edu>
2019-11-11 14:16:50 -0800

    notebooks: Finish simple LDS notebook
    Also, add function to plots.py for visualizing the dynamics of a two-dimensional dynamical
    system.

--- ssm/plots.py
+++ ssm/plots.py
@@ -1,7 +1,12 @@
 import autograd.numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
 def white_to_color_cmap(color, nsteps=256):
+    """Return a cmap which ranges from white to the specified color.
+
+    Ported from HIPS-LIB plotting functions [https://github.com/HIPS/hips-lib]
+    """
     # Get a red-white-black cmap
     cdict = {'red': ((0.0, 1.0, 1.0),
                        (1.0, color[0], color[0])),
@@ -13,7 +18,10 @@ def white_to_color_cmap(color, nsteps=256):
     return cmap
 
 def gradient_cmap(colors, nsteps=256, bounds=None):
-    # Make a colormap that interpolates between a set of colors
+    """Return a colormap that interpolates between a set of colors.
+
+    Ported from HIPS-LIB plotting functions [https://github.com/HIPS/hips-lib]
+    """
     ncolors = len(colors)
     # assert colors.shape[1] == 3
     if bounds is None:
@@ -39,6 +47,10 @@ def gradient_cmap(colors, nsteps=256, bounds=None):
     return cmap
 
 def combo_white_to_color_cmap(colors, nsteps=1000):
+    """Return a colormap that interpolates between white and a set of colors.
+
+    Ported from HIPS-LIB plotting functions [https://github.com/HIPS/hips-lib]
+    """
     ncolors = colors.shape[0]
     # assert colors.shape[1] == 3
     bounds = np.linspace(0,1,ncolors+1)
@@ -59,3 +71,41 @@ def combo_white_to_color_cmap(colors, nsteps=1000):
              'blue': tuple(blues)}
     cmap = LinearSegmentedColormap('white_color_colormap', cdict, nsteps)
     return cmap
+
+def plot_dynamics_2d(dynamics_matrix,
+                     bias_vector,
+                     mins=(-40,-40),
+                     maxs=(40,40),
+                     npts=20,
+                     axis=None,
+                     **kwargs):
+    """Utility to visualize the dynamics for a 2 dimensional dynamical system.
+
+    Args
+    ----
+
+        dynamics_matrix: 2x2 numpy array. "A" matrix for the system.
+        bias_vector: "b" vector for the system. Has size (2,).
+        mins: Tuple of minimums for the quiver plot.
+        maxs: Tuple of maximums for the quiver plot.
+        npts: Number of arrows to show.
+        axis: Axis to use for plotting. Defaults to None, and returns a new axis.
+        kwargs: keyword args passed to plt.quiver.
+
+    Returns
+    -------
+
+        q: quiver object returned by pyplot
+    """
+    assert dynamics_matrix.shape == (2, 2), "Must pass a 2 x 2 dynamics matrix to visualize."
+    assert len(bias_vector) == 2, "Bias vector must have length 2."
+
+    x_grid, y_grid = np.meshgrid(np.linspace(mins[0], maxs[0], npts), np.linspace(mins[1], maxs[1], npts))
+    xy_grid = np.column_stack((x_grid.ravel(), y_grid.ravel(), np.zeros((npts**2,0))))
+    dx = xy_grid.dot(dynamics_matrix.T) + bias_vector - xy_grid
+
+    if axis is not None:
+        q = axis.quiver(x_grid, y_grid, dx[:, 0], dx[:, 1], **kwargs)
+    else:
+        q = plt.quiver(x_grid, y_grid, dx[:, 0], dx[:, 1], **kwargs)
+    return q
