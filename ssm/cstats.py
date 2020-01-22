c83dc1846bca029b90a50be30f7810d7e0d7167a
Benjamin Antin <bantin@stanford.edu>
2020-01-21 18:18:49 -0800

    Cleanup behavenet branch
    Cython syntax go away.

--- ssm/cstats.py
+++ ssm/cstats.py
@@ -1,56 +1,57 @@
 import numpy as np
 
-def robust_ar_statistics(double[:, ::1] Ez,
-                           double[:, :, ::1] tau,
-                           double[:, ::1] x,
-                           double[:, ::1] y,
-                           double[:, :, :, ::1] J,
-                           double[:, :, ::1] h):
+
+def robust_ar_statistics(Ez,
+                         tau,
+                         x,
+                         y,
+                         J,
+                         h):
     raise NotImplementedError
 
 
 # Cython functions to convert between banded and block tridiagonal matrices
-def _blocks_to_bands_lower(double[:,:,::1] Ad, double[:, :, ::1] Aod):
+def _blocks_to_bands_lower(Ad, Aod):
     raise NotImplementedError
 
 
-def _blocks_to_bands_upper(double[:,:,::1] Ad, double[:, :, ::1] Aod):
+def _blocks_to_bands_upper(Ad, Aod):
     raise NotImplementedError
 
 
 # Now do the reverse -- convert bands to blocks
-def _bands_to_blocks_lower(double[:, ::1] A_banded):
+def _bands_to_blocks_lower(A_banded):
     raise NotImplementedError
 
 
-def _bands_to_blocks_upper(double[:,::1] A_banded):
+def _bands_to_blocks_upper(A_banded):
     raise NotImplementedError
 
 
-def _transpose_banded(int l, int u, double[:, ::1] A_banded):
+def _transpose_banded(l, u, A_banded):
     raise NotImplementedError
 
 
-def vjp_cholesky_banded_lower(double[:, ::1] L_bar,
-                                double[:, ::1] L_banded,
-                                double[:, ::1] A_banded,
-                                double[:, ::1] A_bar):
+def vjp_cholesky_banded_lower(L_bar,
+                              L_banded,
+                              A_banded,
+                              A_bar):
     raise NotImplementedError
 
 
-def _vjp_solve_banded_A(double[:, ::1] A_bar,
-                          double[:, ::1] b_bar,
-                          double[:, ::1] C_bar,
-                          double[:, ::1] C,
-                          int u,
-                          double[:, ::1] A_banded):
+def _vjp_solve_banded_A(A_bar,
+                        b_bar,
+                        C_bar,
+                        C,
+                        u,
+                        A_banded):
     raise NotImplementedError
 
 
-def _vjp_solveh_banded_A(double[:, ::1] A_bar,
-                           double[:, ::1] b_bar,
-                           double[:, ::1] C_bar,
-                           double[:, ::1] C,
-                           bint lower,
-                           double[:, ::1] A_banded):
+def _vjp_solveh_banded_A(A_bar,
+                         b_bar,
+                         C_bar,
+                         C,
+                         lower,
+                         A_banded):
     raise NotImplementedError
