fixing test failures by: removing neural nets from laplace-em test cases adding a prior in the AutoRegressiveObservations.initialize() function checking for NaNs in gradient for generic_minimize bumping up number of iterations and lowering tolerance for generic_minimize using more robust likelihood calculation for Bernoulli emissions suppressing log(0) warnings for constrained HMM fixing input stats for AutoRegressiveObservations sufficient statistics adding more reasonable prior on AR parameters (centered on identity transition matrix)

ssm/messages.py
@@ -2,7 +2,6 @@ import numba
 import numpy as np
 import numpy.random as npr
 import scipy.special as scsp
-import scipy.linalg as spla
 from functools import partial
 
 from autograd.tracer import getval
@@ -178,6 +177,12 @@ def hmm_expected_states(pi0, Ps, ll):
     expected_states -= scsp.logsumexp(expected_states, axis=1, keepdims=True)
     expected_states = np.exp(expected_states)
 
+    # Compute the log transition matrices.
+    # Suppress log(0) warnings as they are expected.
+    with np.errstate(divide="ignore"):
+        log_Ps = np.log(Ps)
+
+
     # Compute E[z_t, z_{t+1}] for t = 1, ..., T-1
     # Note that this is an array of size T*K*K, which can be quite large.
     # To be a bit more frugal with memory, first check if the given log_Ps
@@ -187,7 +192,7 @@ def hmm_expected_states(pi0, Ps, ll):
     # M-step is the sum of the expected joints.
     stationary = (Ps.shape[0] == 1)
     if not stationary:
-        expected_joints = alphas[:-1,:,None] + betas[1:,None,:] + ll[1:,None,:] + np.log(Ps)
+        expected_joints = alphas[:-1,:,None] + betas[1:,None,:] + ll[1:,None,:] + log_Ps
         expected_joints -= expected_joints.max((1,2))[:,None, None]
         expected_joints = np.exp(expected_joints)
         expected_joints /= expected_joints.sum((1,2))[:,None,None]
@@ -195,7 +200,7 @@ def hmm_expected_states(pi0, Ps, ll):
     else:
         # Compute the sum over time axis of the expected joints
         expected_joints = np.zeros((K, K))
-        _compute_stationary_expected_joints(alphas, betas, ll, np.log(Ps[0]), expected_joints)
+        _compute_stationary_expected_joints(alphas, betas, ll, log_Ps[0], expected_joints)
         expected_joints = expected_joints[None, :, :]
 
     return expected_states, expected_joints, normalizer

ssm/observations.py
@@ -844,10 +844,9 @@ class AutoRegressiveObservations(_AutoRegressiveObservationsBase):
 
     @ensure_args_are_lists
     def initialize(self, datas, inputs=None, masks=None, tags=None, localize=True):
-        from sklearn.linear_model import LinearRegression
-
         # Sample time bins for each discrete state
         # Use the data to cluster the time bins if specified.
+        K, D, M, lags = self.K, self.D, self.M, self.lags
         Ts = [data.shape[0] for data in datas]
         if localize:
             from sklearn.cluster import KMeans
@@ -867,7 +866,15 @@ class AutoRegressiveObservations(_AutoRegressiveObservationsBase):
             ys = [data[t+self.lags] for t, data in zip(ts, datas)]
 
             # Solve the linear regression
-            coef_, intercept_, Sigma = fit_linear_regression(Xs, ys)
+            J0 = np.diag(np.concatenate((self.l2_penalty_A * np.ones(D * lags),
+                                         self.l2_penalty_V * np.ones(M),
+                                         self.l2_penalty_b * np.ones(1))))
+            h0 = np.concatenate((np.zeros((D * (lags - 1), D)),
+                                 self.l2_penalty_A * np.eye(D),
+                                 np.zeros((M + 1, D))))
+
+            coef_, intercept_, Sigma = fit_linear_regression(
+                Xs, ys, prior_ExxT=J0, prior_ExyT=h0)
             self.As[k] = coef_[:, :self.D * self.lags]
             self.Vs[k] = coef_[:, self.D * self.lags:]
             self.bs[k] = intercept_
@@ -913,6 +920,21 @@ class AutoRegressiveObservations(_AutoRegressiveObservationsBase):
         As = np.zeros((K, D, D * lags + M))
         bs = np.zeros((K, D))
         Sigmas = np.zeros((K, D, D))
+
+        # Set up the prior
+        if J0 is None:
+            J0_diag = np.concatenate((self.l2_penalty_A * np.ones(D * lags),
+                                     self.l2_penalty_V * np.ones(M),
+                                     self.l2_penalty_b * np.ones(1)))
+            J0 = np.tile(np.diag(J0_diag)[None, :, :], (K, 1, 1))
+
+        if h0 is None:
+            h0 = np.concatenate((np.zeros((D * (lags - 1), D)),
+                                 self.l2_penalty_A * np.eye(D),
+                                 np.zeros((M + 1, D))))
+            h0 = np.tile(h0[None, :, :], (K, 1, 1))
+
+        # Collect the data and weights
         xs, ys, Ezs = [], [], []
         for (Ez, _, _), data, input, mask, tag in zip(expectations, datas, inputs, masks, tags):
             if not np.all(mask):
@@ -925,28 +947,16 @@ class AutoRegressiveObservations(_AutoRegressiveObservationsBase):
             )
             ys.append(data[self.lags:])
             Ezs.append(Ez[self.lags:])
-        if continuous_expectations is None:
-            # We are performing an approximate m-step here.
-            # Collect all the data and pass it to fit_linear_regression.
-            if J0 is not None:
-                J = J0
-            else:
-                J_diag = np.concatenate((self.l2_penalty_A * np.ones(D * lags),
-                                        self.l2_penalty_V * np.ones(M),
-                                        self.l2_penalty_b * np.ones(1)))
-                J = np.tile(np.diag(J_diag)[None, :, :], (K, 1, 1))
-            if h0 is not None:
-                h = h0
-            else:
-                h = np.zeros((K, D + M + 1, D))
 
+        if continuous_expectations is None:
+            # Fit each linear regression to the data
             for k in range(K):
                 weights_curr = [Ez[:, k] for Ez in Ezs]
                 A_curr, b_curr, sigma_curr = fit_linear_regression(xs, ys,
                                         weights=weights_curr,
                                         fit_intercept=True,
-                                        prior_ExxT=J[k],
-                                        prior_ExyT=h[k],
+                                        prior_ExxT=J0[k],
+                                        prior_ExyT=h0[k],
                                         Psi0=1,
                                         nu0=1
                                     )
@@ -974,11 +984,11 @@ class AutoRegressiveObservations(_AutoRegressiveObservationsBase):
             ExxT = np.zeros((K, D, D))
             ExyT = np.zeros((K, D, D))
             EyyT = np.zeros((K, D, D))
-            for (ez, (_, ex, smoothed_sigmas, exxn)) in zip(Ezs, continuous_expectations):
+            for ez, (_, ex, smoothed_sigmas, exxn), u in zip(Ezs, continuous_expectations, inputs):
                 for k in range(K):
                     exx = smoothed_sigmas + np.swapaxes(ex[:, None], 2,1) @ ex[:, None]
 
-                    Eu[k] += np.sum(inputs[:-1] * ez[:, k, None], axis=0)
+                    Eu[k] += np.sum(u[:-1] * ez[:, k, None], axis=0)
                     Ex[k] += np.sum(ex[:-1] * ez[:, k, None], axis=0)
                     Ey[k] += np.sum(ex[1:] * ez[:, k, None], axis=0)
                     ExxT[k] += np.sum(exx[:-1, :, :] * ez[:, k, None, None], axis=0)
@@ -990,8 +1000,8 @@ class AutoRegressiveObservations(_AutoRegressiveObservationsBase):
             # E[ xxT xuT x]
             #  [ uxT uuT u]
             #  [ xT   uT 1]
+
             # Dimension of ExxT aug should be (K, D+M+1, D+M+1)
-            u = inputs[:-1]
             for k in range(K):
                 weights_curr = [Ez[:, k] for Ez in Ezs]
                 pz_equal_k = np.sum(weights_curr)
@@ -1015,6 +1025,12 @@ class AutoRegressiveObservations(_AutoRegressiveObservationsBase):
                                         [ExyT[k]],
                                         [Ey[k].T]
                                         ])
+
+                # Add in the prior
+                ExxT_aug += J0[k]
+                ExyT_aug += h0[k]
+
+                # Solve the linear regression
                 A_curr, b_curr, sigma_curr = fit_linear_regression(
                                     xs,
                                     ys,

ssm/optimizers.py
@@ -118,12 +118,19 @@ def _generic_minimize(method, loss, x0, verbose=False, num_iters=1000, state=Non
         itr[0] += 1
         print("Iteration {} loss: {:.3f}".format(itr[0], loss(unflatten(x_flat), -1)))
 
-    # Call the optimizer.
-    # HACK: Pass in -1 as the iteration.
-    result = minimize(_objective, _x0, args=(-1,), jac=grad(_objective),
+    # Wrap the gradient to avoid NaNs
+    def safe_grad(x, itr):
+        g = grad(_objective)(x, itr)
+        g[~np.isfinite(g)] = 1e8
+        return g
+
+    # Call the optimizer.  Pass in -1 as the iteration since it is unused.
+    result = minimize(_objective, _x0, args=(-1,),
+                      jac=safe_grad,
                       method=method,
                       callback=callback if verbose else None,
                       options=dict(maxiter=num_iters, disp=verbose),
+                      tol=1e-4,
                       **kwargs)
     if verbose:
         print("{} completed with message: \n{}".format(method, result.message))

ssm/transitions.py
@@ -42,7 +42,7 @@ class Transitions(object):
         return np.exp(self.log_transition_matrices(data, input, mask, tag))
 
     def m_step(self, expectations, datas, inputs, masks, tags,
-               optimizer="lbfgs", num_iters=100, **kwargs):
+               optimizer="lbfgs", num_iters=1000, **kwargs):
         """
         If M-step cannot be done in closed form for the transitions, default to BFGS.
         """
@@ -109,9 +109,7 @@ class StationaryTransitions(Transitions):
         return np.exp(self.log_Ps - logsumexp(self.log_Ps, axis=1, keepdims=True))
 
     def log_transition_matrices(self, data, input, mask, tag):
-        T = data.shape[0]
         log_Ps = self.log_Ps - logsumexp(self.log_Ps, axis=1, keepdims=True)
-        # return np.tile(log_Ps[None, :, :], (T-1, 1, 1))
         return log_Ps[None, :, :]
 
     def m_step(self, expectations, datas, inputs, masks, tags, **kwargs):
@@ -140,13 +138,15 @@ class ConstrainedStationaryTransitions(StationaryTransitions):
         super(ConstrainedStationaryTransitions, self).__init__(K, D, M=M)
         Ps = self.transition_matrix
         if transition_mask is None:
-            transition_mask = np.ones_like(Ps)
+            transition_mask = np.ones_like(Ps, dtype=bool)
+        else:
+            transition_mask = transition_mask.astype(bool)
 
         # Validate the transition mask. A valid mask must have be the same shape
         # as the transition matrix, contain only ones and zeros, and contain at
         # least one non-zero entry per row.
         assert transition_mask.shape == Ps.shape, "Mask must be the same size " \
-            "as the transition matrix. Found mask of shape {}".format(mask.shape)
+            "as the transition matrix. Found mask of shape {}".format(transition_mask.shape)
         assert np.isin(transition_mask,[1,0]).all(), "Mask must contain only 1s and zeros."
         for i in range(transition_mask.shape[0]):
             assert transition_mask[i].any(), "Mask must contain at least one " \
@@ -155,7 +155,8 @@ class ConstrainedStationaryTransitions(StationaryTransitions):
         self.transition_mask = transition_mask
         Ps = Ps * transition_mask
         Ps /= Ps.sum(axis=-1, keepdims=True)
-        self.log_Ps = np.log(Ps)
+        self.log_Ps = np.log(Ps + LOG_EPS)
+        self.log_Ps[~transition_mask] = -np.inf
 
     def m_step(self, expectations, datas, inputs, masks, tags, **kwargs):
         super(ConstrainedStationaryTransitions, self).m_step(