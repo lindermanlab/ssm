5d1b02c234ba59df9ce9122ea11241e71af9a0d1
mikewojnowicz <mwojnowi@uci.edu>
2023-02-04 09:06:05 -0700

    (maint) added config files for black and isort

--- ssm_star/new/demos/system_driven_transitions/demo_sampling_and_inference_with_SDT.py
+++ ssm_star/new/demos/system_driven_transitions/demo_sampling_and_inference_with_SDT.py
@@ -1,16 +1,11 @@
-import autograd.numpy as np 
+import autograd.numpy as np
 
 import ssm_star
-
+from ssm_star.new import config_util
 from ssm_star.new.generate import generate_regime_sequences_with_runs
+from ssm_star.new.plotting import plot_elbos, plot_results_for_one_entity, plot_sample
 
-from ssm_star.new.plotting import (
-    plot_sample,
-    plot_elbos,
-    plot_results_for_one_entity,
-)
 
-from ssm_star.new import config_util 
 """
 SDT = "System Driven Transitions"
 
@@ -23,18 +18,21 @@ We check that we can
 in each of these two scenarios.
 """
 
-path_to_config="configs/configs_9.yaml"
+path_to_config = "configs/configs_9.yaml"
 CFG = config_util.load(path_to_config)
 
 print(f"\n---{CFG.summary_of_run}---\n")
 
 ###
-# Make system regimes, hard-coded 
+# Make system regimes, hard-coded
 ###
-import numpy as np 
-SYSTEM_REGIMES_INDICES = np.tile(range(CFG.L_true), int(CFG.T))[:CFG.T]
+SYSTEM_REGIMES_INDICES = np.tile(range(CFG.L_true), int(CFG.T))[: CFG.T]
 from lds.util import one_hot_encoded_array_from_categorical_indices
-SYSTEM_REGIMES_ONE_HOT = one_hot_encoded_array_from_categorical_indices(SYSTEM_REGIMES_INDICES, CFG.L_true)
+
+
+SYSTEM_REGIMES_ONE_HOT = one_hot_encoded_array_from_categorical_indices(
+    SYSTEM_REGIMES_INDICES, CFG.L_true
+)
 
 
 ###
@@ -45,43 +43,62 @@ np.random.seed(CFG.seed)
 
 print(f"Sampling SLDS ...")
 slds_for_generation = ssm_star.SLDS(
-    CFG.N, CFG.K_true, CFG.D_true, L=CFG.L_true, 
-    emissions="gaussian", 
-    transitions="system_driven"
+    CFG.N,
+    CFG.K_true,
+    CFG.D_true,
+    L=CFG.L_true,
+    emissions="gaussian",
+    transitions="system_driven",
 )
+
 
 if CFG.use_fixed_cycling_through_regimes_instead_of_Markov_chain_sampling:
     print(f"...with fixed cyclic regimes.")
     fixed_z = generate_regime_sequences_with_runs(CFG.T, CFG.K_true, num_cycles=2)
     z_true, x_true, y = slds_for_generation.sample_with_fixed_z(fixed_z=fixed_z)
 else:
-    print(f"...with system-driven regime transitions. System influence scalar: {CFG.system_influence_scalar:.02f}")
+    print(
+        f"...with system-driven regime transitions. System influence scalar: {CFG.system_influence_scalar:.02f}"
+    )
     slds_for_generation.transitions.Xis *= CFG.system_influence_scalar
-    print(f"Xi, the KxL t.p.m governing how current system regime influences current entity regime is: \n {slds_for_generation.transitions.Xis}")
-    z_true, x_true, y = slds_for_generation.sample(CFG.T, system_input=SYSTEM_REGIMES_ONE_HOT)
+    print(
+        f"Xi, the KxL t.p.m governing how current system regime influences current entity regime is: \n {slds_for_generation.transitions.Xis}"
+    )
+    z_true, x_true, y = slds_for_generation.sample(
+        CFG.T, system_input=SYSTEM_REGIMES_ONE_HOT
+    )
 
 plot_sample(x_true, y, z_true)
 
 
 ###
-# Inference 
+# Inference
 ###
 
 print("Fitting SLDS.")
-slds = ssm_star.SLDS(CFG.N, CFG.K_true, CFG.D_true, L=CFG.L_true, emissions="gaussian", transitions="system_driven", 
-        transition_kwargs=dict(alpha=CFG.alpha, kappa=CFG.kappa))
+slds = ssm_star.SLDS(
+    CFG.N,
+    CFG.K_true,
+    CFG.D_true,
+    L=CFG.L_true,
+    emissions="gaussian",
+    transitions="system_driven",
+    transition_kwargs=dict(alpha=CFG.alpha, kappa=CFG.kappa),
+)
 
 
 ### Warning!  Linderman's initialization seems to assume that the obs dim exceeds the state dim!
 # And if initialization is not done, results are very poor.
 # See: https://github.com/lindermanlab/ssm/blob/646e1889ec9a7efb37d4153f7034c258745c83a5/ssm/lds.py#L161
 if CFG.smart_initialize:
-    slds.initialize(y, num_init_restarts=CFG.num_init_ar_hmms, system_inputs = SYSTEM_REGIMES_ONE_HOT)
+    slds.initialize(
+        y, num_init_restarts=CFG.num_init_ar_hmms, system_inputs=SYSTEM_REGIMES_ONE_HOT
+    )
 
 
 q_elbos, q = slds.fit(
     y,
-    system_inputs = SYSTEM_REGIMES_ONE_HOT,
+    system_inputs=SYSTEM_REGIMES_ONE_HOT,
     method="laplace_em",
     variational_posterior="structured_meanfield",
     initialize=False,
@@ -93,5 +110,5 @@ q_elbos, q = slds.fit(
 ###
 plot_elbos(q_elbos)
 plot_results_for_one_entity(
-    q, slds, y, x_true, z_true, system_input = SYSTEM_REGIMES_ONE_HOT
+    q, slds, y, x_true, z_true, system_input=SYSTEM_REGIMES_ONE_HOT
 )
